4669b7ab6c2a97ca87ac48286aaef4f351f60667
Fahyvor <favourokafor30@gmail.com>
2023-03-13 12:10:05 +0100

    First commit

---
+++ backend/members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: "John Doe",
+        email: "johndoe@gmail.com",
+        status: "active"
+    },
+    {
+        id: 2,
+        name: "Fahyvor",
+        email: "fahyvorokafor@gmail.com",
+        status: "inactive"
+    },
+    {
+        id: 3,
+        name: "Favour Okafor",
+        email: "favourokafor30@gmail.com",
+        status: "active"
+    }
+];
+
+module.exports = members;
